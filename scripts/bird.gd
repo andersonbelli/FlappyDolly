a78b6f6a2e7d77612ed44d91f985a3fb7916f8a2
andersonbelli <ansersonbelli@gmail.com>
2024-08-05 20:37:32 -0300

    adding pipe

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -4,10 +4,6 @@ extends CharacterBody2D
 const SPEED = 420.0
 const FLAP_FORCE = 300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
 func _physics_process(delta):
 	# add gravity
 	if not is_on_floor():
