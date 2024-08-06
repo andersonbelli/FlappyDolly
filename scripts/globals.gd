367ac703ffeed650d86158662a275eeec76cb6e0
andersonbelli <ansersonbelli@gmail.com>
2024-08-06 01:15:23 -0300

    limit repeated pipes up to 3

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -2,3 +2,7 @@ extends Node
 
 var GAME_SPEED = 250
 var SCORE = 0
+
+# Pipes
+var REPEAT_PIPE_COUNT = 0
+var LAST_PIPE_IS_BOTTOM = false
--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -5,15 +5,10 @@ class_name PipeClass
 @onready var up_spawn = $UpSpawn
 @onready var bottom_spawn = $BottomSpawn
 
+var is_bottom = true if (randi_range(0, 1) % 2 == 0) else false
+
 func _ready():
-	var up_or_down = randi_range(0, 1) % 2 == 0
-	position = up_spawn.position
-	
-	if up_or_down:
-		position = bottom_spawn.position
-		position.y += randi_range(0, 250)
-	else:
-		position.y -= randi_range(0, -250)
+	_set_pipe_position()
 
 func _physics_process(delta):
 	if position.x < -450:
@@ -21,3 +16,26 @@ func _physics_process(delta):
 			queue_free()
 	
 	move_and_collide(Vector2(1,0) * delta * Globals.GAME_SPEED * -1)
+
+func _set_pipe_position():
+	position = up_spawn.position
+	
+	if Globals.REPEAT_PIPE_COUNT > 3:
+		Globals.REPEAT_PIPE_COUNT = 0
+		if Globals.LAST_PIPE_IS_BOTTOM:
+			is_bottom = false
+		else:
+			is_bottom = true
+	
+	if Globals.LAST_PIPE_IS_BOTTOM:
+		Globals.REPEAT_PIPE_COUNT += 1
+	else:
+		Globals.REPEAT_PIPE_COUNT += 1
+	
+	if is_bottom:
+		Globals.LAST_PIPE_IS_BOTTOM = true
+		position = bottom_spawn.position
+		position.y += randi_range(0, 250)
+	else:
+		Globals.LAST_PIPE_IS_BOTTOM = false
+		position.y -= randi_range(0, -250)
