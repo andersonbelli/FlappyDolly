0c0fba630c685c0d79c15227ba11aace05ef3169
Belli <ansersonbelli@gmail.com>
2024-08-17 13:40:53 -0300

    Disabling audio in debug mode

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready() -> void:
+	if OS.is_debug_build():
+		_on_audio_button_toggled(true)
+
 func _on_audio_button_toggled(toggled_on: bool) -> void:
 	var bus_idx = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_mute(bus_idx, toggled_on)
