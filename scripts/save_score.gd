609f84dda424b705dd12e3de8ee5346fe8a98355
Belli <ansersonbelli@gmail.com>
2024-08-18 23:32:20 -0300

    Implementing "Button scene" and "Grass scene" to scenes

--- scripts/save_score.gd
+++ scripts/save_score.gd
@@ -2,7 +2,11 @@ extends Control
 
 @onready var line_edit: LineEdit = $VBoxContainer/MarginContainer/LineEdit
 
-func _on_button_pressed() -> void:
+func _on_save_button_pressed() -> void:
 	if line_edit.text.length() >= 3:
 		Globals.PLAYER_NAME = line_edit.text
 		visible = false
+
+
+func _on_close_button_pressed() -> void:
+	pass # Replace with function body.
