92f1ed43e431ef0d92d5cd7a6c0fdc3d53b030b9
Belli <ansersonbelli@gmail.com>
2024-09-17 22:21:24 -0300

    Disabling Save name button and getting score after name input

--- scripts/ranking.gd
+++ scripts/ranking.gd
@@ -7,6 +7,7 @@ var ScoreItem = preload("res://scenes/ui/score_item.tscn")
 
 ## Add your name
 @onready var save_your_score: Control = $SaveYourScore
+@onready var save_button: BaseButton = $SaveYourScore/HBoxContainer/SaveButton
 @onready var line_edit: LineEdit = $SaveYourScore/VBoxContainer/MarginContainer/LineEdit
 
 ## Ranking
@@ -33,16 +34,26 @@ func _ready() -> void:
 		save_your_score.visible = false
 		ranking.visible = true
 
+func _physics_process(delta: float) -> void:
+	if line_edit.text.is_empty():
+		save_button.disabled = true
+	else:
+		save_button.disabled = false
+	
+
 func _on_save_button_pressed() -> void:
 	clear_board()
 	add_loading_scores_message()
 	
 	if line_edit.text.length() > 0:
 		Globals.PLAYER_NAME = line_edit.text
-	save_your_score.queue_free()
-	ranking.visible = true
+		save_your_score.queue_free()
+		ranking.visible = true
 	
-	Globals.save_online_score(Globals.SCORE)
+		Globals.save_online_score(Globals.SCORE)
+		
+		var _ranking = await Globals.load_online_score()
+		_render_board(_ranking)
 
 func _on_close_button_pressed() -> void:
 	close_pressed.emit()
